16b631cc9b7a6f9d8f3fb854276a39e1718fe14e
marwasabon <marwasabon@gmail.com>
2024-08-07 16:45:00 +0000

    Task 13

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,11 @@
+// tempt
+export default function createIteratorObject(report) {
+  let allEmployees = [];
+  for (const item of Object.values(report.allEmployees)) {
+    allEmployees = [
+      ...allEmployees,
+      ...item,
+    ];
+  }
+  return allEmployees;
+}
