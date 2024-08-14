4eb68f91d93c00a129ca85acb1e347b6378b0330
marwasabon <marwasabon@gmail.com>
2024-08-14 11:28:35 +0000

    Task 10

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,15 @@
+// js
+export default function updateUniqueItems(map) {
+  const list = map;
+
+  if (list instanceof Map) {
+    for (const [key, value] of list) {
+      if (value === 1) {
+        list.set(key, 100);
+      }
+    }
+  } else {
+    throw new Error('Cannot process');
+  }
+  return list;
+}
